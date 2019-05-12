229b21463aac2096fb2131dcbd20f3af293e3176
sean <str_ie@yahoo.co.uk>
2019-05-12 11:58:37 +0200

    Add import-sorter example with complex structure

---
+++ import-sorter-multiple-workspaces/complex-project/src/prod/client/MyFile.ts
@@ -0,0 +1,6 @@
+import * as customPackage1 from "custom-package-1";
+import * as customPackage2 from "custom-package-2";
+import * as react from "react";
+
+import { MyLib } from "./lib1/MyLibFile1";
+
